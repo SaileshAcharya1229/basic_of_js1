103096e160ebd0b17f6c4d31a6d40fc26baf4da1
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-07-27 22:20:39 +0545

    some interaction cases

---
+++ assignment1.js
@@ -0,0 +1,8 @@
+const a1 = prompt("enter 1st number");
+const a = Number(a1);
+const b1 = prompt("enter 2nd number");
+const b = Number(b1);
+
+let sum = a + b;
+
+alert(`the sum of two number is ${sum}`);
---
+++ interaction.js
@@ -0,0 +1,14 @@
+//for alert
+alert("hello world");
+
+//for promt
+const age = prompt(" how old are you?");
+
+//for confirm
+const decision = confirm("are you above 18?");
+
+document.write.print(age, decision);
+
+alert(`you are ${age} years old`) //string literal
+
+
