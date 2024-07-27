8b82f6ae5f829102b983404ffe43b15b68e946c3
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-07-27 14:27:41 +0545

    learned about basic variables

--- variables.js
+++ variables.js
@@ -1,4 +1,3 @@
-
 //ES5
 var person1 = "sailesh acharya";
 
@@ -7,3 +6,15 @@ const person2 = "ram";
 let person3 = "shyam";
 
 console.log(person1, person2, person3);
+
+var test = "sailesh";
+var test = "acharya";
+console.log(test);
+
+const name1 = "sailesh"; //fixed variable declaration
+console.log(name1);
+
+let nameperson1 = "sailesh";
+nameperson1 = "acharya"; //can update value of variable
+
+console.log(nameperson1);
